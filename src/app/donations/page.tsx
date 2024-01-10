9e0e4a830ba0d4b04632ab2379df90a7532f2ac9
thomassalduende <thomi44sal@gmail.com>
2024-01-10 17:42:56 -0300

    arreglo de tabla-donations

--- src/app/donations/page.tsx
+++ src/app/donations/page.tsx
@@ -1,5 +1,15 @@
 import { TableDonations } from "@/components/TableDonations";
 
 export default function Donations() {
-    return <TableDonations />
+    return (
+        <section className=' container m-auto grid min-h-screen grid-rows-[auto, 1fr, auto] px-4 font-sans antialiased'>
+            <header className='text-xl font-bold leading-[4rem]'>Doname Algo Rata</header>
+            <main className='py-8'>
+                <TableDonations />
+            </main>
+            <footer className='text-center leading-[4rem] opacity-70'>
+                © {new Date().getFullYear()} ThomasSalduende
+            </footer>
+        </section>
+    )
 }
--- src/components/TableDonations.tsx
+++ src/components/TableDonations.tsx
@@ -14,7 +14,7 @@ export const TableDonations = async () => {
     const donations = await supabase.from('donations').select('*').then(({ data }) => data as unknown as Promise<Donations>)
 
     return (
-        <div className="max-w-md mx-auto bg-white p-6 rounded-lg shadow-md">
+        <div className="max-w-[600px] mx-auto bg-black p-6 rounded-lg shadow-md">
             <h2 className="text-2xl mb-4">Donaciones</h2>
             <table className="min-w-full">
                 <thead>
@@ -22,14 +22,24 @@ export const TableDonations = async () => {
                         <th className="py-2 px-4">ID</th>
                         <th className="py-2 px-4">Message</th>
                         <th className="py-2 px-4">Amount</th>
+                        <th className="py-2 px-4">Fecha</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {donations.map((item, index) => (
-                        <tr key={index} className={(index % 2 === 0) ? 'bg-gray-100' : 'bg-white'}>
+                    {donations.map((item) => (
+                        <tr key={item.id}>
                             <td className="py-2 px-4">{item.id}</td>
                             <td className="py-2 px-4">{item.message}</td>
-                            <td className="py-2 px-4">{item.amount.toLocaleString('es-AR', { style: 'currency', currency: 'ARS' })}</td>
+                            <td className="py-2 px-4 ">{item.amount}</td>
+                            <td className="py-2 px-4">{new Date(item.created_at).toLocaleString('es-ES', {
+                                year: 'numeric',
+                                month: 'long',
+                                day: 'numeric',
+                                hour: '2-digit',
+                                minute: '2-digit',
+                                second: '2-digit',
+                                timeZone: 'UTC'
+                            })}</td>
                         </tr>
                     ))}
                 </tbody>
