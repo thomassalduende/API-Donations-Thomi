4843c23b6b4a300a2565b35849d923758cfcfde6
DevThomasSalduende <tsalduende@joobpay.com>
2024-04-13 19:27:25 -0300

    probando

--- src/components/FormDonation.tsx
+++ src/components/FormDonation.tsx
@@ -26,23 +26,22 @@ export function FormDonation() {
                 back_urls: {
                     success: 'https://api-donations-thomi.vercel.app/',
                     pending: 'https://api-donations-thomi.vercel.app/',
-                    failure: 'https://api-donations-thomi.vercel.app/'
+                    failure: 'https://api-donations-thomi.vercel.app'
                 },
-                // payer: {
-                //     name: 'Enzo Bua',
-                //     phone: {
-                //         area_code: '3446',
-                //         number: '377934'
-                //     },
-                //     email: 'enzobua86@gmail.com',
-                // },
+                payer: {
+                    name: 'Enzo Bua',
+                    phone: {
+                        area_code: '3446',
+                        number: '377934'
+                    },
+                    email: 'enzobua86@gmail.com',
+                },
                 auto_return: 'approved',
                 notification_url: 'https://1c8c-190-17-57-6.ngrok-free.app/pagos',
                 payment_methods: {
                     installments: 6,
                     excluded_payment_methods: [
-                        { id: 'credit_card' },
-                        { id: 'prepaid_card' }
+                        { id: 'visa' }
                     ],
                 }
             }
